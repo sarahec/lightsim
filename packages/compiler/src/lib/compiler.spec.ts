592e6d1a5475f5613de7878497a96308808c06e9
Sarah Clark <seclark@nextquestion.net>
2023-04-23 15:22:33 -0700

    Export a `compile` command

--- packages/compiler/src/lib/compiler.spec.ts
+++ packages/compiler/src/lib/compiler.spec.ts
@@ -1,7 +1,19 @@
-import { compiler } from './compiler';
+import { VFile } from 'vfile';
+import { compile, FileFormat } from './compiler';
 
 describe('compiler', () => {
-  it('should work', () => {
-    expect(compiler()).toEqual('compiler');
+  it('Should do nothing if no pages', () => {
+    expect(compile([], FileFormat.HTML)).toEqual([]);
+  });
+  it('Should compile a single page', () => {
+    const source = new VFile({
+      path: 'test.md',
+      value: '# Hello World!',
+    });
+    const expected = new VFile({
+      path: 'page1.html',
+      value: '<h1>Hello World!</h1>',
+    });
+    expect(compile([source], FileFormat.HTML)).toEqual([expected]);
   });
 });
--- packages/compiler/src/lib/compiler.ts
+++ packages/compiler/src/lib/compiler.ts
@@ -1,3 +1,28 @@
-export function compiler(): string {
-  return 'compiler';
+import { VFile } from 'vfile';
+import { Logger, ILogObj } from 'tslog';
+import { splitFile } from './paginator';
+import { toHTML, toMarkdown } from './rendering';
+
+export enum FileFormat {
+  HTML = 'html',
+  Markdown = 'md',
+}
+
+/**
+ * Compiles and formats a list of VFiles.
+ *
+ * @param sources Markdown pages to be compiled
+ * @param format Output format
+ * @returns New VFiles with the compiled content (potentially multiple per source)
+ */
+export function compile(sources: VFile[], format: FileFormat): VFile[] {
+  const log: Logger<ILogObj> = new Logger();
+  const outputs: VFile[] = [];
+  const formatter = format == FileFormat.HTML ? toHTML : toMarkdown;
+  for (const source of sources) {
+    log.trace(`Compiling ${source.path}`);
+    const pages = splitFile(source);
+    outputs.push(...pages.map((page) => formatter(page)));
+  }
+  return outputs;
 }
