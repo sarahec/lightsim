37ba9ba3615a40a0dfb1a1163e12dbc5291b7b9b
Sarah Clark <seclark@nextquestion.net>
2023-04-30 12:23:15 -0700

    Corner case when no matches

--- packages/compiler/src/lib/encapsulate.spec.ts
+++ packages/compiler/src/lib/encapsulate.spec.ts
@@ -14,15 +14,15 @@
  limitations under the License.
  */
 
- import { Processor, unified } from 'unified';
+import { Processor, unified } from 'unified';
 
 import { group } from './encapsulate';
 import { type Node } from 'unist';
 import { u } from 'unist-builder';
 
 const matchFn = (probe: Node) =>
-// @ts-expect-error heading nodes always have a depth
-probe.type === 'heading' && probe.depth === 2;
+  // @ts-expect-error heading nodes always have a depth
+  probe.type === 'heading' && probe.depth === 2;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const wrapFn = (nodes: Node[], count?: number, matched?: Node) => ({
@@ -34,31 +34,16 @@ let h2Tree: Node;
 let paginatedH2Tree: Node;
 
 beforeEach(() => {
-  configuredProcessor = unified()
-    .use(group, { match: matchFn, wrap: wrapFn });
-  h2Tree = u('root', [  
-    u('heading', { depth: 2 }, [
-      u('text', 'Hello'),
-    ]),
-    u('heading', { depth: 2 }, [
-      u('text', 'World'),
-    ]),
+  configuredProcessor = unified().use(group, { match: matchFn, wrap: wrapFn });
+  h2Tree = u('root', [
+    u('heading', { depth: 2 }, [u('text', 'Hello')]),
+    u('heading', { depth: 2 }, [u('text', 'World')]),
   ]);
   paginatedH2Tree = u('root', [
-    u('page', [
-      u('heading', { depth: 2 }, [
-        u('text', 'Hello'),
-      ]),
-    ]),
-    u('page', [
-      u('heading', { depth: 2 }, [
-        u('text', 'World'), 
-      ]),
-    ]),
+    u('page', [u('heading', { depth: 2 }, [u('text', 'Hello')])]),
+    u('page', [u('heading', { depth: 2 }, [u('text', 'World')])]),
   ]);
 });
-
-
 
 describe('group plugin', () => {
   it('Should do nothing if empty tree', async () => {
@@ -69,53 +54,55 @@ describe('group plugin', () => {
   });
 
   it('Should do nothing if no match', async () => {
-    const tree = u('root', [
-      u('heading', { depth: 1 }, [
-        u('text', 'Hello'),
-      ]),
-    ]);
+    const tree = u('root', [u('heading', { depth: 1 }, [u('text', 'Hello')])]);
     const expected = tree;
     const result = configuredProcessor.runSync(tree);
     expect(result).toEqual(expected);
   });
-
 });
 
 describe('match option', () => {
   it('should accept a match function', () => {
-    const result = unified().use(group, { match: matchFn, wrap: wrapFn }).runSync(h2Tree);
+    const result = unified()
+      .use(group, { match: matchFn, wrap: wrapFn })
+      .runSync(h2Tree);
     expect(result).toEqual(paginatedH2Tree);
   });
 
   it('should accept a node type', () => {
-    const result = unified().use(group, { match: "heading", wrap: wrapFn }).runSync(h2Tree);
+    const result = unified()
+      .use(group, { match: 'heading', wrap: wrapFn })
+      .runSync(h2Tree);
     expect(result).toEqual(paginatedH2Tree);
   });
 
   it('should accept a partial node', () => {
-    const result = unified().use(group, { match: u('heading', {depth: 2}), wrap: wrapFn }).runSync(h2Tree);
+    const result = unified()
+      .use(group, { match: u('heading', { depth: 2 }), wrap: wrapFn })
+      .runSync(h2Tree);
     expect(result).toEqual(paginatedH2Tree);
   });
-
 });
-
 
 describe('wrap option', () => {
   it('should accept a wrap function', () => {
-    const result = configuredProcessor.use(group, {match: matchFn, wrap: wrapFn }).runSync(h2Tree);
+    const result = configuredProcessor
+      .use(group, { match: matchFn, wrap: wrapFn })
+      .runSync(h2Tree);
     expect(result).toEqual(paginatedH2Tree);
   });
 
   it('should accept a node type', () => {
-    const result = unified().use(group, { match: matchFn, wrap: 'page' }).runSync(h2Tree);
+    const result = unified()
+      .use(group, { match: matchFn, wrap: 'page' })
+      .runSync(h2Tree);
     expect(result).toEqual(paginatedH2Tree);
   });
 
   it('should accept a partial node', () => {
-    const result = unified().use(group, {match: matchFn, wrap: {type: 'page'} }).runSync(h2Tree);
+    const result = unified()
+      .use(group, { match: matchFn, wrap: { type: 'page' } })
+      .runSync(h2Tree);
     expect(result).toEqual(paginatedH2Tree);
   });
-
 });
-
-
