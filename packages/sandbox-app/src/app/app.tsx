6e5a15f24265a6d39efcd36506627264005d46ab
Sarah Clark <seclark@nextquestion.net>
2023-05-10 21:12:54 -0700

    Prepare the sandbox for development

--- packages/sandbox-app/src/app/app.tsx
+++ packages/sandbox-app/src/app/app.tsx
@@ -1,13 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
+// import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="sandbox-app" />
-
+      {/* <NxWelcome title="sandbox-app" /> */}
+      <h1>This is a test of the emergency broadcast system</h1>
+      {/* TODO Add content for the sandbox */}
       <div />
     </>
   );
