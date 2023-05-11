4d2fc5135482e8939d22b17bc95f86c0b2e2719e
Sarah Clark <seclark@nextquestion.net>
2023-05-10 21:21:20 -0700

    Scaffold rollup plugin

---
+++ packages/rollup-plugin-lightsim/src/lib/rollup-plugin-lightsim.spec.ts
@@ -0,0 +1,7 @@
+import { rollupPluginLightsim } from './rollup-plugin-lightsim';
+
+describe('rollupPluginLightsim', () => {
+  it('should work', () => {
+    expect(rollupPluginLightsim()).toEqual('rollup-plugin-lightsim');
+  });
+});
