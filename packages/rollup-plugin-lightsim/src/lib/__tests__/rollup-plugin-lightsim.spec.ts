c684abf328cfd92a16e432a71c52b9accea07a06
Sarah Clark <seclark@nextquestion.net>
2023-05-15 08:58:40 -0700

    Basic rollup plugin

---
+++ packages/rollup-plugin-lightsim/src/lib/__tests__/rollup-plugin-lightsim.spec.ts
@@ -0,0 +1,88 @@
+// Copyright 2023 Sarah Clark
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { expect, jest } from '@jest/globals';
+import { compile } from '@lightsim/compiler';
+import { glob } from 'glob';
+import { read } from 'to-vfile';
+import { VFile } from 'vfile';
+import { rollupPluginLightsim } from '../rollup-plugin-lightsim';
+
+describe('rollupPluginLightsim', () => {
+  // Generate a Jest mock function for glob
+  // https://jestjs.io/docs/mock-functions#mocking-modules
+
+  beforeAll(() => {
+    // @ts-expect-error glob is a mock function
+    glob.mockResolvedValue(['content/test.md']);
+  });
+
+  it('should generate a plugin', async () => {
+    const plugin = await rollupPluginLightsim('content/test.md');
+    expect(plugin).toHaveProperty('name');
+    expect(plugin).toHaveProperty('buildStart');
+    expect(plugin).toHaveProperty('watchChange');
+  });
+
+  it('should add files to watch', async () => {
+    const plugin = await rollupPluginLightsim('content/test.md');
+
+    const addWatchFile = jest.fn();
+    // @ts-expect-error buildStart is a function and thus is callable
+    plugin.buildStart?.call({ addWatchFile });
+    expect(addWatchFile).toHaveBeenCalledWith('content/test.md');
+  });
+
+  it('should compile files on watch change', async () => {
+    const source = new VFile({ path: 'content/test.md', value: '# Test' });
+    const compiled = new VFile({
+      path: 'test.html',
+      value: '<h1>Test</h1>',
+    });
+
+    const plugin = await rollupPluginLightsim('content/test.md');
+    // @ts-expect-error read is a mock function
+    read.mockResolvedValue(source);
+    // @ts-expect-error compile is a mock function
+    compile.mockResolvedValue([compiled]);
+
+    const emitFile = jest.fn();
+    // @ts-expect-error watchChange is a function and thus is callable
+    await plugin.watchChange?.call({ emitFile }, 'content/test.md');
+
+    expect(emitFile).toHaveBeenCalledWith({
+      type: 'asset',
+      fileName: 'test.html',
+      source: '<h1>Test</h1>',
+    });
+  });
+});
+
+jest.mock('glob', () => {
+  return {
+    glob: jest.fn(),
+  };
+});
+
+jest.mock('to-vfile', () => {
+  return {
+    read: jest.fn(),
+  };
+});
+
+jest.mock('@lightsim/compiler', () => {
+  return {
+    compile: jest.fn(),
+  };
+});
--- packages/rollup-plugin-lightsim/src/lib/rollup-plugin-lightsim.spec.ts
+++
@@ -1,7 +0,0 @@
-import { rollupPluginLightsim } from './rollup-plugin-lightsim';
-
-describe('rollupPluginLightsim', () => {
-  it('should work', () => {
-    expect(rollupPluginLightsim()).toEqual('rollup-plugin-lightsim');
-  });
-});
