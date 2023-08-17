29fac79aa459c9345fede4ef30b3e89cbfe657b6
sarahec <sarahec@nextquestion.net>
2023-08-16 21:22:27 -0700

    Fix unexpected token errors

--- packages/api/jest.config.ts
+++ packages/api/jest.config.ts
@@ -1,4 +1,7 @@
 /* eslint-disable */
+// Modules throwing error "Jest encountered an unexpected token"
+const esModules = ['unified', 'remark-parse', 'vfile'];
+
 export default {
   displayName: 'api',
   preset: '../../jest.preset.js',
@@ -6,6 +9,9 @@ export default {
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
   },
+  transformIgnorePatterns: [
+    `<rootDir>/node_modules/(?!.*\\.mjs$)`,
+  ],
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/api'
 };
